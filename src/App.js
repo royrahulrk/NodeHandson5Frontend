dabe58758f6f074028ac40c4842712eacee2f83f
tarunshxrma <itssharmatarun@gmail.com>
2023-05-18 22:13:07 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-backend-qfcv.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
